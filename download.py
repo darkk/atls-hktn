b0b0b728a036f6eb1ef4b585de8b50143007f614
Leonid Evdokimov <leon@darkk.net.ru>
2017-02-11 22:53:17 +0300

    Tactical downloading -- maximise variance of measurements

--- download.py
+++ download.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 
 import ujson
+import random
 import sys
 from subprocess import Popen, PIPE
 import requests
@@ -11,34 +12,77 @@ from datadict import *
 
 def download(url, dest):
     assert not os.path.exists(dest)
-    print 'Downloading', dest
-    with tempfile.NamedTemporaryFile(prefix='tmpmsm', dir='.') as rawfd:
-        proc = Popen(['lz4', '-5'], stdin=PIPE, stdout=rawfd)
-        req = requests.get(url, stream=True, timeout=15.)
-        for blob in req.iter_content(chunk_size=16*1024, decode_unicode=False):
-            proc.stdin.write(blob)
-        proc.stdin.flush()
-        proc.stdin.close()
-        proc.wait()
-        if proc.returncode != 0:
-            raise RuntimeError('lz4 failed', proc.returncode)
-        os.link(rawfd.name, dest)
+    proc = None
+
+    try:
+        print 'Downloading', dest
+        with tempfile.NamedTemporaryFile(prefix='tmpmsm', dir='.') as rawfd:
+            proc = Popen(['lz4', '-5'], stdin=PIPE, stdout=rawfd)
+            req = requests.get(url, stream=True, timeout=15.)
+            for blob in req.iter_content(chunk_size=16*1024, decode_unicode=False):
+                proc.stdin.write(blob)
+                stat = os.statvfs('.')
+                stat = stat.f_bavail * 1. / stat.f_blocks
+                if stat < 0.01:
+                    raise RuntimeError('Less than 2% free, see `df -h`', stat)
+            proc.stdin.flush()
+            proc.stdin.close()
+            proc.wait()
+            if proc.returncode != 0:
+                raise RuntimeError('lz4 failed', proc.returncode)
+            os.link(rawfd.name, dest)
+    except Exception as exc:
+        print 'Failed', dest, '->', str(exc)
+        if proc is not None:
+            try:
+                proc.kill()
+            except Exception:
+                pass
+            try:
+                proc.wait()
+            except Exception:
+                pass
 
 def main():
-    fetchlist = []
+    fetchlist = {}
     for doc in sys.stdin:
         doc = ujson.loads(doc) # NB: trailing whitespace is not a big deal for `ujson`
         if doc['type']['name'] in ('ping', 'traceroute'):
             src = map(str, (p['id'] for p in doc['probes'] if p['id'] in geoprb))
-            url = 'https://atlas.ripe.net/api/v2/measurements/{}/results/?start={}&probe_ids={}&format=txt'.format(
-                        doc['msm_id'], 1483228800, ','.join(src))
             dest = 'msm/{}.json.lz4'.format(doc['msm_id'])
             if not os.path.exists(dest):
-                fetchlist.append( (url, dest) )
+                stop_time = doc['stop_time']
+                if stop_time is None:
+                    stop_time = 1485820800 # Jan 31 2017
+                stop_time = min(stop_time, 1485820800)
+
+                start_time = doc['start_time']
+                while start_time < stop_time:
+                    thisstop = min(stop_time, start_time + 24*3600)
+                    dest = 'msm/{}-{}-{}.json.lz4'.format(doc['msm_id'], start_time, thisstop)
+                    url = 'https://atlas.ripe.net/api/v2/measurements/{}/results/?start={}&stop={}&probe_ids={}&format=txt'.format(
+                                doc['msm_id'], start_time, thisstop, ','.join(src))
+                    start_time = thisstop
+                    if not os.path.exists(dest):
+                        fetchlist.setdefault(doc['msm_id'], []).append( (url, dest) )
+                    else:
+                        print 'Skipping', dest
             else:
                 print 'Skipping', dest
+    for k in fetchlist.keys():
+        random.shuffle(fetchlist[k])
+    flattened = []
+    while len(fetchlist):
+        msm_id = random.choice(fetchlist.keys())
+        pair = random.choice(fetchlist[msm_id])
+        flattened.append(pair)
+        fetchlist[msm_id].remove(pair)
+        if not len(fetchlist[msm_id]):
+            del fetchlist[msm_id]
+            if len(fetchlist) % 100 == 0:
+                print 'Remaining unscheduled measurements...', len(fetchlist)
     pool = mp.Pool(processes=8)
-    pool.map(lambda x: download(*x), fetchlist, chunksize=1)
+    pool.map(lambda x: download(*x), flattened, chunksize=1)
     pool.close()
     pool.join()
 
